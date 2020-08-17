6ec9cf2caea8935a2e635c68d50ff2b0ced0d0ab
Alex <sasha-a@ukr.net>
2020-08-18 00:09:54 +0300

    Windows project is done

---
+++ src/js/modules/modals.js
@@ -0,0 +1,111 @@
+const modals = (state) => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]'),
+              scroll = calcScroll();
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+
+                showModal();
+
+                function hideModal() {
+                    windows.forEach(item => {
+                        item.style.display = 'none';
+                    });
+                }
+
+                function showModal() {
+                    modal.style.display = 'block';
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scroll}px`;
+                    // document.body.classList.add('modal-open');
+                }
+            
+                if (e.target.classList.contains('popup_calc_button')) {
+                    if (!state.form || !state.height || !state.width) {
+                        console.log('Please, input all data');
+                        modal.style.display = 'none';
+                    } else {
+                        hideModal();
+                        showModal();
+                    }
+                }
+
+                if (e.target.classList.contains('popup_calc_profile_button')) {
+                    if (!state.profile || !state.type) {
+                        console.log('Please, check type of window');
+                        modal.style.display = 'none';    
+                    } else {
+                        hideModal();
+                        showModal();
+                    }
+                }
+                
+                state.form = 1;
+                state.type = "tree";
+                
+            });
+        });
+        
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+            // document.body.classList.remove('modal-open');
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
+                // document.body.classList.remove('modal-open');
+            }
+        });
+    }
+
+    function showModalByTime(selector, time) {
+        setTimeout(function() {
+            document.querySelector(selector).style.display = 'block';
+        }, time);
+    }
+
+    function calcScroll() {
+        const div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+        document.body.appendChild(div);
+
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+    showModalByTime('.popup', 600000);
+    
+};
+
+export default modals;
